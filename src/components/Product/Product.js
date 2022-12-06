07c8b5b018ab996b83339e15e09aeda7c94f7ebb
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-06 23:35:30 +0530

    added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';           
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
-    // console.log(props.product)
     const {name, img, seller, price, ratings} = props.product;
-    // console.log(img)
+    // console.log(props)
     return (
         <div className='product'>
             <img src={img} alt=""></img>
-            <p className="product-name">{name}</p>
-            {/* <h2> product name : {name} </h2> */}
+            <div className='product-info'>
+                <p className="product-name">{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            {/* add to cart button with set up wrapped so not be automanic   OutPut */}
+            <button onClick={ () => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></p>
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,12 +5,22 @@ import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products,setProducts] = useState([])
+    const [products,setProducts] = useState([])//first stats for container cart
+     // stats for container cartContainer 
+     const [cart, setCart] = useState([]);//second stats for container cart
     useEffect( ()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+
+    // button from event handler products 
+    const handleAddToCart = (product) =>{
+        // cart push product
+        const newCart = [...cart,product]
+        // console.log(product)
+        setCart(newCart);
+    } 
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -18,11 +28,13 @@ const Shop = () => {
                 products.map(product => <Product 
                     key={product.id}
                     product ={product}
+                    handleAddToCart={handleAddToCart} //button eventHandler 
                     ></Product>)
                }
             </div>
-            <div className="cart-container">
+            <div className="cart-container"> {/* button eventHandler */}
                <h4>order summery</h4>
+               <p>Selected Items: {cart.length} </p>
             </div>
         </div>
     );
